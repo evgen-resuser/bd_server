3c93ce02cdecb7f7d7e2f8d215087a6d7486d886
e.syroezhkin <e.syroezhkin@g.nsu.ru>
2024-04-25 16:09:23 +0700

    PlaceId equals and hashcode

--- src/main/java/org/evgen/bd_server/model/place/PlaceId.java
+++ src/main/java/org/evgen/bd_server/model/place/PlaceId.java
@@ -11,6 +11,9 @@ import java.io.Serializable;
 @Embeddable
 public class PlaceId implements Serializable {
 
+    private int id;
+    private int typeId;
+
     public PlaceId(){}
 
     public PlaceId(int typeId) {
@@ -23,7 +26,13 @@ public class PlaceId implements Serializable {
         this.typeId = typeId;
     }
 
-    private int id;
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id+typeId);
+    }
 
-    private int typeId;
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
